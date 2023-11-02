4333a1efa4b6954a2446ee1dd75a1b0ed0912ffa
sharow yeh <yuyu.yei@hotmail.com>
2023-11-02 17:00:38 +0800

    add build script for cl.exe(msvc) with vcpkg change pkgconf args to calling script instead of build file

---
+++ build_rubberband_cl_x64.bat
@@ -0,0 +1,27 @@
+ECHO ensure using x64 or x86_64 command prompt for visual studio to run this batch in windows environment
+
+::: ensure run this script at the script directory
+cd /d %~dp0
+
+ECHO %VCPKG_ROOT%
+
+if [%VCPKG_ROOT%] == [] ECHO VCPKG_ROOT must be assigned && exit /b
+
+::: check vcpkg and pkgconfig related environment for meson.build usage
+::: ensure the pkgconf is from vcpkg instead of cygwin
+SET PKG_CONFIG=%VCPKG_ROOT%\installed\x64-windows\tools\pkgconf\pkgconf.exe
+ECHO %PKG_CONFIG%
+
+::: x64 release
+mkdir build-x64-rel
+
+meson setup build-x64-rel --wipe -Dprefix=%~dp0 -Dpkg_config_path=%VCPKG_ROOT%\installed\x64-windows\lib\pkgconfig -Dextra_include_dirs=%VCPKG_ROOT%\installed\x64-windows\include -Dextra_lib_dirs=%VCPKG_ROOT%\installed\x64-windows\lib -Dfft=fftw -Dresampler=libsamplerate -Dbuildtype=release 
+
+::: x64 debug
+mkdir build-x64-dbg
+
+meson setup build-x64-dbg --wipe -Dprefix=%~dp0 -Dpkg_config_path=%VCPKG_ROOT%\installed\x64-windows\lib\pkgconfig -Dextra_include_dirs=%VCPKG_ROOT%\installed\x64-windows\include -Dextra_lib_dirs=%VCPKG_ROOT%\installed\x64-windows\debug\lib -Dfft=fftw -Dresampler=libsamplerate -Dbuildtype=debug 
+
+
+::: ninja -C build-x64-rel
+ninja -C build-x64-dbg
